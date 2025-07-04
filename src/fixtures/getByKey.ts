c316e6c5dc4c12ed7d001e153a9bcca437f5e904
chlbri <51156116+chlbri@users.noreply.github.com>
2025-07-04 22:37:43 +0000

    ok

--- src/fixtures/getByKey.ts
+++
@@ -1,8 +0,0 @@
-import { decompose } from '@bemedev/decompose';
-import type { Fn } from 'types';
-
-export const getByKey = (obj: any, key: string) => {
-  const func = decompose as unknown as Fn;
-  const decomposed = func(obj);
-  return decomposed[key];
-};
